2a7bea2c205b45de8bc9338ce367c096e86decc2
unknown <henritejasuranto@gmail.com>
2021-08-27 03:46:48 +0800

    Question C2

--- animal/src/main/java/com/singtel/animal/vo/Butterfly.java
+++ animal/src/main/java/com/singtel/animal/vo/Butterfly.java
@@ -1,6 +1,36 @@
 package com.singtel.animal.vo;
 
+import com.singtel.animal.enums.ButterflyStage;
+
 public class Butterfly extends FlyAnimal {
+	private ButterflyStage stage;
+	
+	public Butterfly() {
+		
+	}
+	
+	public Butterfly(ButterflyStage stage) {
+		this.stage = stage;
+	}
+	
+	public void walk() {
+		if(stage == ButterflyStage.Caterpilar) {
+			System.out.println("I can walk (crawl)");
+		}
+		else {
+			System.out.println("I cannot walk");
+		}
+	}
+	
+	public void fly() {
+		if(stage == ButterflyStage.Caterpilar) {
+			System.out.println("I cannot fly");
+		}
+		else {
+			System.out.println("I can fly");
+		}
+	}
+	
 	public void sound() {
 		System.out.println("I does not make a sound");
 	}
--- animal/src/test/java/com/singtel/animal/vo/ButterflyTest.java
+++ animal/src/test/java/com/singtel/animal/vo/ButterflyTest.java
@@ -4,18 +4,52 @@ import static org.mockito.Matchers.eq;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.internal.verification.VerificationModeFactory;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import com.singtel.animal.enums.ButterflyStage;
+
 @RunWith(PowerMockRunner.class)
 public class ButterflyTest extends BaseTest {
-	@InjectMocks
-	private Butterfly butterfly;
+	@Test
+	public void test_caterpilar_walk() throws Exception {
+		Butterfly caterpilar = new Butterfly(ButterflyStage.Caterpilar);
+		caterpilar.walk();
+		Mockito.verify(printStream, VerificationModeFactory.times(1)).println(eq("I can walk (crawl)"));
+	}
 
 	@Test
-	public void test_sound() throws Exception {
+	public void test_caterpilar_fly() throws Exception {
+		Butterfly caterpilar = new Butterfly(ButterflyStage.Caterpilar);
+		caterpilar.fly();
+		Mockito.verify(printStream, VerificationModeFactory.times(1)).println(eq("I cannot fly"));
+	}
+
+	@Test
+	public void test_caterpilar_sound() throws Exception {
+		Butterfly caterpilar = new Butterfly(ButterflyStage.Caterpilar);
+		caterpilar.sound();
+		Mockito.verify(printStream, VerificationModeFactory.times(1)).println(eq("I does not make a sound"));
+	}
+
+	@Test
+	public void test_butterfly_walk() throws Exception {
+		Butterfly butterfly = new Butterfly(ButterflyStage.Butterfly);
+		butterfly.walk();
+		Mockito.verify(printStream, VerificationModeFactory.times(1)).println(eq("I cannot walk"));
+	}
+
+	@Test
+	public void test_butterfly_fly() throws Exception {
+		Butterfly butterfly = new Butterfly(ButterflyStage.Butterfly);
+		butterfly.fly();
+		Mockito.verify(printStream, VerificationModeFactory.times(1)).println(eq("I can fly"));
+	}
+
+	@Test
+	public void test_butterfly_sound() throws Exception {
+		Butterfly butterfly = new Butterfly(ButterflyStage.Butterfly);
 		butterfly.sound();
 		Mockito.verify(printStream, VerificationModeFactory.times(1)).println(eq("I does not make a sound"));
 	}
